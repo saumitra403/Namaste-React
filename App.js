db734f7201f5f3a2058951f5c171e054e649c431
saumitra403 <mishra.saumitra06@gmail.com>
2023-05-23 09:58:13 +0530

    Used React with HTML

---
+++ App.js
@@ -0,0 +1,14 @@
+//the second empty object are the attributes of a tag
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Im an h1 tag"),
+    React.createElement("h2", {}, "Im an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Im an h1 tag"),
+    React.createElement("h2", {}, "Im an h2 tag"),
+  ]),
+]); //ReactElement (Object) => HTML(Browser Understands)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
---
+++ README.md
@@ -0,0 +1,1 @@
+# Repository for Practicing React
