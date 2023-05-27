4e2e255d362fe5ea59ef4b09dfc5037ad020d1af
saumitra403 <mishra.saumitra06@gmail.com>
2023-05-27 23:13:33 +0530

    lesson 2

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 //the second empty object are the attributes of a tag
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Im an h1 tag"),
+    React.createElement("h1", {}, "This is namaste React"),
     React.createElement("h2", {}, "Im an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Repository for Practicing React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - Written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification of files
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking
+- Different dev and prod bundles
