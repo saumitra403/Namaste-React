44105d7a319202f9045b64219657fc25b35bdf97
saumitra403 <mishra.saumitra06@gmail.com>
2023-06-18 00:15:58 +0530

    Added basic skeleton of food app

--- App.js
+++ App.js
@@ -1,51 +1,96 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1",{"id": "heading"},"Namaste React")
+/*
+  Header 
+    - Logo
+    - Nav Items
 
-//JSX - HTML like syntax
+  Body
+    - Search
+    - Restaurant Container
+      - Restaurant Card
+        - Img
+        - Name of Res, Star Rating, cuisines, delivery time
 
-//JSX => Converted to React.createElement - JS Object => HTMLElement(render)
+  Footer
+    - Copyright
+    - Links
+    - Address
+    - Contact
+*/
 
-//JSX Element
-// const heading = <h1 id="heading">Namaste React using JSX</h1>;
+const styleCard = {
+  backgroundColor: "#f0f0f0"
+}
 
-//React Component
-//Functional Components - Javascript function that returns JSX/React Element
-const element = <span>React Element</span>;
-
-//Component Compositions
-
-const title = (
-  <h1 className="head" tabIndex="5">
-    Namaste React using JSX
-  </h1>
-);
-
-//React Fragment - behaves like an empty tag. Its same as <></>
-const HeadingComponent = () => {
+const Header = () => {
   return (
-    <>
-      <div id="container">
-        {title}
-        {/* Babel takes care of Cross Site Scripting here */}
-        {/* {Title()} Another cool way of writing jsx */}
-        <h1 id="heading"> Namaste React Functional Component</h1>
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        ></img>
       </div>
-      <div id="container2"></div>
-    </>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
-const Title = () => {
+const RestaurantCard = () => {
   return (
-    <h1 className="head" tabIndex="5">
-      Namaste React using JSX
-    </h1>
+    <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
+      <img alt="res-logo" className="res-logo" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/uf5cux6pxuwtb59ql0zx"></img>
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  )
+}
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+      </div>
+    </div>
+  )
+}
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header></Header>
+      <Body></Body>
+    </div>
   );
 };
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(HeadingComponent); Cannot render like this
-
-root.render(<HeadingComponent />);
+root.render(<AppLayout></AppLayout>);
