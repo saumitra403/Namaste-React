ad66ed3264dde6095ffc80acd86dfc34cc6af68d
saumitra403 <mishra.saumitra06@gmail.com>
2023-09-18 13:21:44 +0530

    Added Tailwind

--- src/components/User.js
+++ src/components/User.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const User = (props) => {
     const [count] = useState(0);
     const [count2] = useState(1);
-    return <div className="user-card">
+    return <div className="m-4 p-4 bg-gray-50 rounded-lg">
         <h1>Count: {count}</h1>
         <h1>Count2: {count2}</h1>
         <h2>Name: {props.name}</h2>
