7f8bebc573a3b6566aeff9cd52efb6735905c091
saumitra403 <mishra.saumitra06@gmail.com>
2023-10-08 22:53:24 +0530

    Modified CSS

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,19 +1,26 @@
+import { useState } from "react";
 import ItemList from "./ItemList";
 
 const RestaurantCategory = ({ data }) => {
+  const [showItems, setShowItems] = useState(false);
+  const handleClick = () => {
+    setShowItems(!showItems);
+  };
   return (
     <div>
       {/* Header */}
       <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4">
-        <div className="flex justify-between">
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={handleClick}
+        >
           <span className="font-bold text-lg">
             {data?.title} ({data?.itemCards.length})
           </span>
           <span>⬇ </span>
         </div>
-        <ItemList items={data?.itemCards} />
+        {showItems && <ItemList items={data?.itemCards} />}
       </div>
-      {/* Accordion Body */}
     </div>
   );
 };
